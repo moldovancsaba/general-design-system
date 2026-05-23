52b2b9819c6642d0a0c4bed13e7cead742934f94
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2026-05-23 15:05:46 +0200

    fix: dedupe react and mantine

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/general-design-system/'
+  base: '/general-design-system/',
+  resolve: {
+    dedupe: ['react', 'react-dom', '@mantine/core', '@mantine/hooks']
+  }
 })
